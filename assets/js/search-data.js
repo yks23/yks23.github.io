f9948ab25a0e8a0e5708b1a7b8d53b3959271c12
yks23 <157187169+yks23@users.noreply.github.com>
2026-06-27 02:05:42 +0000

    Deploying to gh-pages from @ yks23/yks23.github.io@92cb6171ded04a1f0fc37ec31896929855a3aae0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -45,18 +45,7 @@ ninja.data = [{
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
-      },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-awarded-the-wangdazhong-scholarship-20000-rmb",
+      },{id: "news-awarded-the-wangdazhong-scholarship-20000-rmb",
           title: '🏆 Awarded the WangDazhong Scholarship (20000 RMB).',
           description: "",
           section: "News",},{id: "news-our-paper-tree-kg-has-been-accepted-to-acl-2025",
